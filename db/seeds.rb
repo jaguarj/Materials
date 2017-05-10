acc63b72a85ceb4c0932f8546d655ce1be6511c9
Josh Seipel <joshseipel@gmail.com>
2017-05-09 23:52:59 -0400

    Removed favorite icon, comments, and share buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Shirt.create(name: "Circles", image: ('https://cdn.shopify.com/s/files/1/1245/4197/products/circlesmock_grande.png?v=1484253165'), bio: "This shirt was made in NYC by NYC Design.", url: ('https://shop.oldfashionedrobot.com/'))
-Shirt.create(name: "Diamond", image: ('https://shop.oldfashionedrobot.com/products/green-shiny'), bio: "This Diamond shirt was made in NYC by NYC Design.", url: ('https://shop.oldfashionedrobot.com/'))
-Shirt.create(name: "Shirt", image: ('https://cdn.shopify.com/s/files/1/1245/4197/products/blackshirtmock_1000x1000.png?v=1484342375'), bio: "This 'Shirt' was made in NYC by NYC Design.", url: ('https://shop.oldfashionedrobot.com/'))
+# Shirt.create(name: "Circles", image: ('https://cdn.shopify.com/s/files/1/1245/4197/products/circlesmock_grande.png?v=1484253165'), bio: "This shirt was made in NYC by NYC Design.", url: ('https://shop.oldfashionedrobot.com/'))
+# Shirt.create(name: "Diamond", image: ('https://shop.oldfashionedrobot.com/products/green-shiny'), bio: "This Diamond shirt was made in NYC by NYC Design.", url: ('https://shop.oldfashionedrobot.com/'))
+# Shirt.create(name: "Shirt", image: ('https://cdn.shopify.com/s/files/1/1245/4197/products/blackshirtmock_1000x1000.png?v=1484342375'), bio: "This 'Shirt' was made in NYC by NYC Design.", url: ('https://shop.oldfashionedrobot.com/'))
 
+# Shirt.create(name: "Sunset Chasers", image: ('https://s-media-cache-ak0.pinimg.com/originals/f8/66/80/f86680a78c9a702223559f10da3701c7.jpg'), bio: "Made by seeloveclothing", url: ('https://seeloveclothing.com/products/sunset-chasers-tee'))
+
+# Shirt.create(name: "Pocket Cat", image: ('https://s-media-cache-ak0.pinimg.com/564x/aa/70/ff/aa70ff871314ee81739b78f5c7d566b6.jpg'), bio: "Made by teepublic.com", url: ('https://www.teepublic.com/t-shirt/818498-cute-cat-in-a-pocket-is-so-kawaii?store_id=103145'))
+
+# Shirt.create(name: "Miami Beach Society", image: ('https://img1.etsystatic.com/041/1/5732185/il_570xN.561579865_2rye.jpg'), bio: "Vintage '88 MBS", url: ('https://www.etsy.com/listing/178625432/vintage-1988-miami-beach-society-palm?ref=shop_home_active_7'))
+
+# Shirt.create(name: "Music Note", image: ('https://s-media-cache-ak0.pinimg.com/564x/a8/61/7b/a8617b1fc26f4360628320203ad69979.jpg'), bio: "Saint Laurent Music", url: ('http://www.polyvore.com/saint_laurent_music_note-print_crew-neck/thing?.embedder=21532983&.src=share_desktop&.svc=pinterest&id=192549473&utm_campaign=default'))
+
+# Shirt.create(name: "Hiking", image: ('https://s-media-cache-ak0.pinimg.com/564x/05/49/aa/0549aabbbce6860918542ff2bfe25b0c.jpg'), bio: "designbyhumans", url: ('https://www.designbyhumans.com/shop/t-shirt/men/hiking/126586/'))
+
+# Shirt.create(name: "Mountain Bear", image: ('data:image/gif;base64,R0lGODlhDQAJAIAAAAAAAAAAACH5BAEAAAAALAAAAAANAAkAAAIJhI+py+0Po4QFADs='), bio: "designbyhumans", url: ('https://www.designbyhumans.com/shop/t-shirt/men/mountain-bear/162804/'))
+# Shirt.create(name: "Double Vision", image: ('https://s-media-cache-ak0.pinimg.com/564x/ea/9e/3d/ea9e3d242584de4e0d145edefa61355a.jpg'), bio: "designbyhumans", url: ('https://www.designbyhumans.com/shop/t-shirt/men/double-vision/25229/'))
+# Shirt.create(name: "Tycho", image: ('https://s-media-cache-ak0.pinimg.com/originals/0c/e3/ca/0ce3ca8bade0c87b775e53496c01379e.png'), bio: "Tycho Music", url: ('https://www.pinterest.com/pin/417920040396271986/'))
+# Shirt.create(name: "Perception", image: ('https://s-media-cache-ak0.pinimg.com/originals/1e/69/ec/1e69ec0c1279bf3234e89551d908d1f0.png'), bio: "UGMONK", url: ('https://shop.ugmonk.com/products/perception-white?utm_campaign=Pinterest%20Buy%20Button&utm_medium=Social&utm_source=Pinterest&utm_content=pinterest-buy-button-195ef48c8-7b3a-48ca-92d9-9cc9e157c0d2'))
