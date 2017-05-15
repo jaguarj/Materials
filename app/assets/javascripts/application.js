2bb61410d7b55e15761a35f63a4a3bfe0c5ba321
Josh Seipel <joshseipel@gmail.com>
2017-05-15 11:38:37 -0400

    Adding socialshare to app, testing now.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require angular/angular
 //= require angular-ui-router/release/angular-ui-router
+//= require angular-socialshare
 //= require_self
 //= require_tree .
 
